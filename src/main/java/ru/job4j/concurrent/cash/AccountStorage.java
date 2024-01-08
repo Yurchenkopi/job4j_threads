14c26cc01f0dfd97223c5ce715a867c421da4cea
Pavel <heppie@yandex.ru>
2024-01-08 14:07:47 +0300

    3. Денежные переводы AccountStorage [#1104 #471353]

--- src/main/java/ru/job4j/concurrent/cash/AccountStorage.java
+++ src/main/java/ru/job4j/concurrent/cash/AccountStorage.java
@@ -8,45 +8,33 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        accounts.put(account.id(), account);
-        return accounts.containsKey(account.id());
+        return account != null && accounts.putIfAbsent(account.id(), account) == null;
     }
 
     public synchronized boolean update(Account account) {
-        accounts.computeIfPresent(account.id(), (k, v) -> account);
-        return accounts.get(account.id()).amount() == account.amount();
+        return accounts.replace(account.id(), accounts.get(account.id()), account);
     }
 
     public synchronized void delete(int id) {
-        if (accounts.containsKey(id)) {
-            accounts.put(id, null);
-            accounts.remove(id);
-        }
+        accounts.remove(id);
     }
 
     public synchronized Optional<Account> getById(int id) {
-        return accounts.values().stream()
-                .filter(a -> a.id() == id)
-                .findFirst();
+        return Optional.ofNullable(accounts.get(id));
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         boolean rsl = false;
         if (
-                accounts.containsKey(fromId)
-                        && accounts.containsKey(toId)
+                getById(fromId).isPresent() && getById(toId).isPresent()
                         && amount <= accounts.get(fromId).amount()
         ) {
             Account fromAcc = accounts.get(fromId);
             Account toAcc = accounts.get(toId);
             int fromAccAmount = fromAcc.amount() - amount;
             int toAccAmount = toAcc.amount() + amount;
-            accounts.computeIfPresent(fromId, (k, v) -> new Account(fromId, fromAccAmount));
-            accounts.computeIfPresent(toId, (k, v) -> new Account(toId, toAccAmount));
-            rsl =
-                    fromAcc.amount() - accounts.get(fromId).amount() == amount
-                    &&
-                    accounts.get(toId).amount() - toAcc.amount() == amount;
+            rsl = update(new Account(fromId, fromAccAmount))
+                    && update(new Account(toId, toAccAmount));
         }
         return rsl;
     }
