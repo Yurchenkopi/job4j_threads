0690f5f5f897698acc66afca3234e44e44bed1ba
Pavel <heppie@yandex.ru>
2024-01-10 22:16:49 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/concurrent/CountBarrier.java
@@ -0,0 +1,62 @@
+package ru.job4j.concurrent;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            if (count >= total) {
+                monitor.notifyAll();
+            } else {
+                count++;
+            }
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountBarrier countBarrier = new CountBarrier(10);
+        Thread master = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    for (int i = 0; i <= countBarrier.count; i++) {
+                        System.out.printf("count = %d%s",
+                                countBarrier.count, System.lineSeparator());
+                        countBarrier.count();
+                    }
+                },
+                "Master"
+        );
+        Thread slave = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName() + " started");
+                },
+                "Slave"
+        );
+        master.start();
+        slave.start();
+        master.join();
+        slave.join();
+        System.out.println("Finished");
+    }
+}
---
+++ src/main/java/ru/job4j/concurrent/examples/SemaphoreEx.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent.examples;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreEx {
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(0);
+        Runnable task = () -> {
+            try {
+                semaphore.acquire();
+                System.out.println("Нить выполнила задачу");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(task).start();
+        Thread.sleep(3000);
+        semaphore.release(1);
+    }
+}
